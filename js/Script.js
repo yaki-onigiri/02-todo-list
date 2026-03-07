146e0f93608b69ec089d3f04ef735ea610954103
yaki-onigiri <117330897+ yaki-onigiri@users.noreply.github.com>
2026-03-07 19:31:12 +0900

    TODOアプリに削除機能を追加、タスクの見栄えを改善

--- js/Script.js
+++ js/Script.js
@@ -3,12 +3,25 @@ const button = document.getElementById("add-button");
 const list = document.getElementById("todo-list");
 
 button.addEventListener("click", function (){
-    const text = input.value;
+    const text = input.value.trim();
 
     if (text ==="") return;
 
     const li = document.createElement("li");
-    li.textContent = text;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    checkbox.addEventListener("change", function(){
+        if(checkbox.checked){
+            span.classList.add("completed");
+        }else{
+            span.classList.remove("completed");
+        }
+    });
 
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "削除";
@@ -17,6 +30,8 @@ button.addEventListener("click", function (){
         li.remove();
     });
 
+    li.appendChild(checkbox);
+    li.appendChild(span);
     li.appendChild(deleteButton);
 
     list.appendChild(li);
@@ -24,7 +39,7 @@ button.addEventListener("click", function (){
     input.value = "";
 });
 
-input.addEventListener("keypress", function(event){
+input.addEventListener("keydown", function(event){
 
     if(event.key === "Enter"){
         button.click();
